187963fa0141e3eaa439126c4d960d96d823a203
HectorCruz27 <cc22056@ues.edu.sv>
2025-10-28 22:43:53 -0600

    Agregando cambios en frontend, creacion de dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    loadComponent: () => import('./components/layout/layout').then(m => m.default),
+    children: [
+        {
+            path: 'dashboard',
+            loadComponent: () => import('./business/dashboard/dashboard').then(m => m.default),
+
+        },
+         {
+            path: 'profile',
+            loadComponent: () => import('./business/profile/profile').then(m => m.default),
+
+        },
+         {
+            path: 'tables',
+            loadComponent: () => import('./business/tables/tables').then(m => m.default),
+
+        },
+        {
+            path: '',
+            redirectTo: 'dashboard',
+            pathMatch: 'full'
+        }
+
+]
+
+        },
+        {
+            path: '**',
+            redirectTo: 'dashboard'
+
+        }
+];
